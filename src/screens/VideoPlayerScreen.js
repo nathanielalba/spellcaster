678adf14c540c9c7fa02bd38a1e9412a8d0117f1
Nathaniel Alba <nathaniel.alba@gmail.com>
2020-08-20 10:55:37 -0700

    basic scaffold

---
+++ src/screens/VideoPlayerScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const VideoPlayerScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>VideoPlayerScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default VideoPlayerScreen;
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+/**
+ * Main app theme's configuration
+ */
+
+export default {
+  colors: {
+    primary: '#3f4953',
+    secondary: '#5d666e',
+    white: '#fff',
+    black: '#000',
+  },
+};
